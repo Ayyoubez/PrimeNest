607b578383455dd21937675c128dbc695d003469
Ayyoubez <ayyoubez@gmail.com>
2025-08-30 16:14:36 +0300

    completed Projects section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import About from "./Components/About";
 import Header from "./Components/Header";
 import Navbar from "./Components/Navbar";
+import Projects from "./Components/Projects";
 
 const App = () => {
   return (
     <div>
       <Header />
       <About />
+      <Projects />
     </div>
   );
 };
